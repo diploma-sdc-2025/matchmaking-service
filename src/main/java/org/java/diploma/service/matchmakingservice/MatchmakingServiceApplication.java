7ec50a77b71f379fc11945030417c42e41e0b448
Konstantin <konstantin.kernazhytski@gmail.com>
2025-11-23 18:46:38 +0200

    init commit

---
+++ src/main/java/org/java/diploma/service/matchmakingservice/MatchmakingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.java.diploma.service.matchmakingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchmakingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchmakingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/java/diploma/service/matchmakingservice/MatchmakingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.diploma.service.matchmakingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmakingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
